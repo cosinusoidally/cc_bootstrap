e1d9c9273ac7439f36408a178db63c79638f8e70
Liam Wilson <cosinusoidally@gmail.com>
2023-10-15 14:48:51 +0100

    fix test.js

--- 0001_initial/test.js
+++ 0001_initial/test.js
@@ -1,9 +1,13 @@
+print_orig=print;
+print=function(){};
 loadRelativeToScript("support.js");
 loadRelativeToScript("main.js");
 
 argc_argv=mk_argc_argv(["out2.exe", "0"]);
 print(JSON.stringify(argc_argv));
 main(argc_argv.argc,argc_argv.argv);
+
+print=print_orig;
 
 print(vfs["stdout.txt"].map(function(x){return String.fromCharCode(x)}).join(""));
 
