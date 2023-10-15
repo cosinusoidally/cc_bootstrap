af94139bd02cab9bf0d1c7b72bac10786a197ffd
Liam Wilson <cosinusoidally@gmail.com>
2023-10-15 17:22:19 +0100

    more

--- 0001_initial/test.js
+++ 0001_initial/test.js
@@ -1,10 +1,13 @@
 var arg;
+if(!arg){
+  arg="0";
+}
 print_orig=print;
 print=function(){};
 loadRelativeToScript("support.js");
 loadRelativeToScript("main.js");
 
-argc_argv=mk_argc_argv(["out2.exe", "0"]);
+argc_argv=mk_argc_argv(["out2.exe", arg]);
 print(JSON.stringify(argc_argv));
 main(argc_argv.argc,argc_argv.argv);
 
